40c36ef6c3578e7d930873239dd5e643c985f7dd
Grzegorz Łucki <mrgretwon@gmail.com>
2017-09-08 15:54:39 +0200

    .

--- rest_api/teo_api/tests.py
+++ rest_api/teo_api/tests.py
@@ -3,5 +3,5 @@ from django.test import TestCase
 class YourTestClass(TestCase):
 
     def test_false_is_true(self):
-        print("Method: test_true_is_true.")
+        print("Nie dzialaja testy i nie wiem dlaczego.")
         self.assertTrue(True)
